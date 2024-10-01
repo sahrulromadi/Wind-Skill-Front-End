705ee8b1258fce477b85b7e94a984f03c380fc98
Sahrul Romadi <sahrulromadi12@gmail.com>
2024-10-01 12:33:43 +0700

    membuat struktur project

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <h1 className="text-5xl font-bold">Hello World</h1>
+      {/* Navbar */}
+      <Navbar />
+
+      {/* Home Page */}
+      <HomePage />
+
+      {/* Footer */}
+      <Footer />
     </>
   );
 }
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold text-teal-100">Hello World</h1>;
+    </>
+  );
+}
+
+export default HomePage;
