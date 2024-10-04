6b6a32fa1cf925bc37c093787db661fd9854a0a9
Sahrul Romadi <sahrulromadi12@gmail.com>
2024-10-04 13:08:03 +0700

    membuat hamburger menu dan mengatur usestate agar ketika di klik maka menu nya akan muncul

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [show, setShow] = useState(true);
+  const menuClick = () => {
+    setShow(!show);
+    console.log(show);
+  };
+
+  let menuActive = () => {
+    if (show) {
+      return "left-0";
+    } else {
+      return "-left-full";
+    }
+  };
+
   return (
     <nav className="navbar fixed w-full py-4">
       <div className="container mx-auto px-6">
@@ -14,25 +29,34 @@ const Navbar = () => {
             />
           </div>
           {/* Content */}
-          <ul className="flex lg:gap-14 md:static md:bg-transparent md:w-auto md:h-full md:translate-y-0 md:flex-row md:text-black md:shadow-none md:p-0 md:m-0 gap-8 fixed left-0 top-1/2 -translate-y-1/2 flex-col p-8 rounded shadow-lg shadow-slate-300 bg-sky-500 font-bold text-white">
-            <li className="font-medium opacity-65 hover:text-black md:hover:text-sky-500">
+          <ul
+            className={`flex gap-6 fixed ${menuActive()} top-1/2 -translate-y-1/2 flex-col ps-4 pe-6 py-6 rounded shadow-lg shadow-slate-300 bg-sky-500 font-bold text-white transition-all md:static md:bg-transparent md:w-auto md:h-full md:translate-y-0 md:flex-row md:text-black md:shadow-none md:p-0 md:m-0 lg:gap-14`}
+          >
+            <li className="font-medium opacity-65 hover:text-black md:hover:text-sky-500 flex items-center gap-2">
+              <i className="ri-home-2-line text-2xl block md:hidden"></i>
               <a href="http://">Beranda</a>
             </li>
-            <li className="font-medium opacity-65 hover:text-black md:hover:text-sky-500">
+            <li className="font-medium opacity-65 hover:text-black md:hover:text-sky-500 flex items-center gap-2">
+              <i className="ri-information-line text-2xl block md:hidden"></i>
               <a href="http://">Tentang Kami</a>
             </li>
-            <li className="font-medium opacity-65 hover:text-black md:hover:text-sky-500">
+            <li className="font-medium opacity-65 hover:text-black md:hover:text-sky-500 flex items-center gap-2">
+              <i className="ri-settings-line text-2xl block md:hidden"></i>
               <a href="http://">Layanan</a>
             </li>
-            <li className="font-medium opacity-65 hover:text-black md:hover:text-sky-500">
+            <li className="font-medium opacity-65 hover:text-black md:hover:text-sky-500 flex items-center gap-2">
+              <i className="ri-image-line text-2xl block md:hidden"></i>
               <a href="http://">Kontak</a>
             </li>
           </ul>
           {/* Login */}
-          <div className="login bg-sky-500 border border-sky-500 text-white px-5 py-2 rounded-full ml-auto hover:bg-white hover:text-sky-500 hover:border hover:border-sky-500">
+          <div className="login bg-sky-500 border border-sky-500 text-white px-5 py-2 rounded-full ml-auto md:ml-0 hover:bg-white hover:text-sky-500 hover:border hover:border-sky-500">
             <a href="http://">Login</a>
           </div>
-          <i className="ri-menu-3-line text-2xl block ml-6 md:hidden"></i>
+          <i
+            className="ri-menu-3-line text-2xl block ml-6 md:hidden"
+            onClick={menuClick}
+          ></i>
         </div>
       </div>
     </nav>
